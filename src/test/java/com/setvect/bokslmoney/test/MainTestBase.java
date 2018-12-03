a9648a0fd9f273774517ac9a4370f0ae16255eda
setvect <setvect@naver.com>
2018-12-03 23:35:14 +0900

    마이그레이션 - 작업중

--- src/test/java/com/setvect/bokslmoney/test/MainTestBase.java
+++ src/test/java/com/setvect/bokslmoney/test/MainTestBase.java
@@ -30,4 +30,47 @@ public class MainTestBase {
 	static {
 		System.setProperty(BokslMoneyConstant.TEST_CHECK_PROPERTY_NAME, "true");
 	}
+
+	/** 초기 데이터 입력 */
+	private static final String INIT_INSERT_SQL = "/init.sql";
+
+	/** 엔티티의 refrash, merge 등을 관리하기 위해 */
+	@Autowired
+	private EntityManager entityManager;
+
+	/**
+	 * DB 초기 값 등록.
+	 */
+	protected void insertInitValue() {
+
+	}
+
+	/**
+	 * 텍스트에서 쿼리문을 문장 단위로 분리
+	 *
+	 * @param text
+	 *            sql문을 담고 있는 텍스트
+	 * @return 문장 단위 쿼리문
+	 */
+	private List<String> extractQeruy(final String text) {
+		List<String> result = new ArrayList<>();
+		StringBuffer r = new StringBuffer();
+		String[] sqlInsertQueries = text.split("\n");
+
+		for (String s : sqlInsertQueries) {
+			if (StringUtils.isBlank(s)) {
+				continue;
+			}
+			if (s.startsWith("--")) {
+				continue;
+			}
+			r.append(s);
+			if (s.trim().endsWith(";")) {
+				result.add(r.toString());
+				r.setLength(0);
+			}
+		}
+		return result;
+	}
+
 }
