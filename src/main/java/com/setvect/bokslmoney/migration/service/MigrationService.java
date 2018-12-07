a2f2c315c8cfb2223eb5dbbd0bba6e38a02d567a
setvect <setvect@naver.com>
2018-12-07 23:47:17 +0900

    마이그레이션 - 작업중

--- src/main/java/com/setvect/bokslmoney/migration/service/MigrationService.java
+++ src/main/java/com/setvect/bokslmoney/migration/service/MigrationService.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,10 @@ import com.setvect.bokslmoney.code.vo.CodeItemKey;
 import com.setvect.bokslmoney.code.vo.CodeItemVo;
 import com.setvect.bokslmoney.code.vo.CodeMainVo;
 import com.setvect.bokslmoney.hab.repository.AccountRepository;
+import com.setvect.bokslmoney.hab.repository.CategoryRepository;
 import com.setvect.bokslmoney.hab.vo.AccountVo;
+import com.setvect.bokslmoney.hab.vo.CategoryVo;
+import com.setvect.bokslmoney.hab.vo.KindType;
 
 @Service
 public class MigrationService {
@@ -29,6 +34,9 @@ public class MigrationService {
 	@Autowired
 	private CodeMainRepository codeMainRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	public void run() {
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -38,6 +46,8 @@ public class MigrationService {
 
 		try {
 			String url = "jdbc:sqlite:./temp/setvect.db";
+
+			// 1. 계좌 코드
 			conn = DriverManager.getConnection(url);
 			ps = conn.prepareStatement("select  * from t_acctype");
 			rs = ps.executeQuery();
@@ -56,6 +66,7 @@ public class MigrationService {
 			rs.close();
 			ps.close();
 
+			// 2. 계좌 정보
 			ps = conn.prepareStatement("select  * from t_account");
 			rs = ps.executeQuery();
 			// accountRepository.deleteAll();
@@ -73,6 +84,40 @@ public class MigrationService {
 				account.setNote(rs.getString("a_memo"));
 				accountRepository.saveAndFlush(account);
 			}
+			rs.close();
+			ps.close();
+
+			// 3. 항목 코드
+			ps = conn.prepareStatement("select  * from t_item order by it_key");
+			rs = ps.executeQuery();
+			Map<Integer, KindType> kindMap = new HashMap<>();
+			kindMap.put(0, KindType.SPENDING);
+			kindMap.put(1, KindType.INCOME);
+			kindMap.put(2, KindType.TRANSFER);
+
+			Map<Integer, CategoryVo> parentMap = new HashMap<>();
+			while (rs.next()) {
+				int key = rs.getInt("it_key");
+				int highKey = rs.getInt("it_highkey");
+				int isincome = rs.getInt("it_isincome");
+				int order = rs.getInt("it_order");
+				String name = rs.getString("it_name");
+
+				CategoryVo category = new CategoryVo();
+				category.setKind(kindMap.get(isincome));
+				category.setName(name);
+				category.setOrderNo(order);
+
+				if (highKey == -1) {
+					category.setParentSeq(0);
+				} else {
+					CategoryVo parent = parentMap.get(highKey);
+					category.setParentSeq(parent.getCategorySeq());
+				}
+				categoryRepository.saveAndFlush(category);
+				parentMap.put(key, category);
+			}
+
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		} finally {
