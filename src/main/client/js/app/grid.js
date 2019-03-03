ad023bdd8d13c92955215f70a860a95e36d86af1
setvect <setvect@naver.com>
2019-03-03 17:08:14 +0900

    webpack 적용 - 작업중(표형태로 내역 입력)

--- src/main/client/js/app/grid.js
+++ src/main/client/js/app/grid.js
@@ -1,16 +1,15 @@
 import Vue from "vue";
 import gridApp from "../../component/transaction/grid.vue";
-console.log("#################");
 let EventBus = new Vue();
 Vue.prototype.$EventBus = EventBus;
 Vue.use(VeeValidate, {
 	locale: "ko",
 	events: "blur"
 });
+
 new Vue({
 	el: "#app",
 	render: h => h(gridApp),
 	mounted() {
-		console.log("DDDDDDDDDDDDDDDDDDDDDD");
 	}
 });
