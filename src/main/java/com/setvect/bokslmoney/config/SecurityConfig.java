fa07c6ef7bddc2bf4947a37723c1b4c2595d08f3
setvect <setvect@naver.com>
2019-05-31 08:56:18 +0900

    PBM_20190531: 메모 등록 오류 수정

--- src/main/java/com/setvect/bokslmoney/config/SecurityConfig.java
+++ src/main/java/com/setvect/bokslmoney/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.setvect.bokslmoney.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
